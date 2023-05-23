982757612f9230e70c2ef4cb687d531d6d8cff17
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-05-23 09:57:29 -0300

    Ajustes

--- Tip/Depois/index.js
+++ Tip/Depois/index.js
@@ -1,12 +1,12 @@
 function formatMoney(value) {
     value = Math.ceil(value * 100) / 100
     value = value.toFixed(2)
-    return '$ ' + value 
+    return ' R$ ' + value 
 } 
 
 function formatSplit(value) {
-    if (value == 1) return value + ' person'
-    return value + ' people'
+    if (value == 1) return value + ' Pessoa '
+    return value + ' Pessoas '
 }
 
 
